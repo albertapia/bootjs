559e3b8da077d7c17a354681e2b662005cdba588
Alberto <albert-@live.com.ar>
2023-05-17 23:33:31 -0300

    funcion salarios

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
-let anioAct = prompt("Por favor ingrsar Año Actual");
-let anioNac = prompt("Por favor ingresar tu año de nacimiento");
+let salAnual = prompt("Por favor Ingrese su Salario Anual");
+let hsPorDia = prompt("Por favor Ingrese Cuantas Horas trabaja por dia");
 
 function anios () {
-    console.log(anioAct);
-    console.log(anioNac);
+    console.log("Salario Anual: " + salAnual);
+    let salMen = salAnual/12;
+    console.log("Salario Mensual: " + salMen);
+    let salSem = salMen/4;
+    console.log("Salario Semanal: " + salSem);
+    let salDia = salSem/7;
+    console.log("Salario Diario: " + salDia);
+    console.log("Salario por Hora: " + salDia / hsPorDia);
+
 }
 
 anios();
