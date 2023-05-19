c77c150ccf31fd3f09e3042ad303924f03540996
Alberto <albert-@live.com.ar>
2023-05-19 12:38:47 -0300

    armando el ejercicios del 19/5 salarios pidiendo datos por input y trabajarlos

--- amd.js
+++ amd.js
@@ -4,17 +4,6 @@ let dia = prompt("Ingrese el dia (formato DD): ");
 
 let AMD = anio + mes + dia; //AMD acronimo Año Mes Dia
 
-function errorDate (mes, dia) {
-    if (mes == 0 || mes > 12) {
-        alert("Mes invalido"); 
-    }
-
-    if (dia > 31) {
-        alert ("Dia Invalido");
-    }
-}
-
-errorDate();
 document.write(AMD);
 console.log(AMD);
 
---
+++ salariosFunciones.js
@@ -0,0 +1,13 @@
+const numeroIntegrantes = prompt("Cuantos integrantes tiene tu familia?");
+const numeroTrabajadores = prompt("Cuantos Integrantes de tu familia Trabajan")
+
+const salarios = [];
+
+for (i = 0; i < numeroTrabajadores; i++){
+    const salario = prompt ("Ingresar el salario del trabajador " + i);
+}
+
+
+// asi se sacaba mayor y menor
+//const mayorSalario = Math.max(...salarios);
+//const menorSalario = Math.min(...salarios);
