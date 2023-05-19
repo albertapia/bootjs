1d1945e056db499bc03fdf5d5d6147f9f744f04f
Alberto <albert-@live.com.ar>
2023-05-19 20:09:07 -0300

    buscabdo la vuelta save antes de borrar todo

--- salariosFunciones.js
+++ salariosFunciones.js
@@ -1,13 +1,44 @@
-const numeroIntegrantes = prompt("Cuantos integrantes tiene tu familia?");
-const numeroTrabajadores = prompt("Cuantos Integrantes de tu familia Trabajan")
+const numIntegrantes = prompt("Cuantos integrantes tiene tu familia?");
 
 const salarios = [];
 
-for (i = 0; i < numeroTrabajadores; i++){
-    const salario = prompt ("Ingresar el salario del trabajador " + i);
+for (let i = 0; i < numTrabajadores; i++) {
+  const salario = prompt(`Ingrese el salario del trabajador ${i + 1}:`);
+  salarios.push(salario);
 }
 
+function mayorSalario (...salarios) {
+    let maxSalario = 0;
+    for (let i = 0; i < salarios.length; i++) {
 
-// asi se sacaba mayor y menor
-//const mayorSalario = Math.max(...salarios);
-//const menorSalario = Math.min(...salarios);
+        let sumaSalarios = sumaSalarios + salarios.salario;
+
+        if (salarios.salario > maxSalario) {
+            maxSalario = salarios.salario;
+            }
+        }
+        document.write(maxSalario);
+        document.write(sumaSalarios);
+        document.write(sumaSalarios / numTrabajadores)
+    }
+function menorSalario (...salarios){
+    let minSalario = null;
+
+    for (let i = 0; i < salarios.length; i++) {
+        
+        if (salarios.salario > minSalario) {
+            minSalario = salarios.salario;
+            }
+        }
+        document.write(minSalario);
+    }
+
+
+//function crearParrafo(){
+ //   let salario = document.querySelector("#salario").value;
+ //   let parrafo = document.createElement('p');
+ //   let capturaDiv = document.querySelector('#mensaje');
+ //   capturaDiv.appendChild(parrafo);
+ //   parrafo.textContent = salario ;
+//}
+//console.log(salario);
