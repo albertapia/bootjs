2512e0c380a5f4c7e8cadfca09c3a6c99b121228
Alberto <albert-@live.com.ar>
2023-05-20 20:37:37 -0300

    se creo el boton para cargar el form luego del bucle

--- salarioFinal.js
+++ salarioFinal.js
@@ -1,27 +1,30 @@
 //let numeroIntegrantes = prompt("Cuantos Integrantes tiene tu Familia?");
 
-let cantidadTrabajando= document.querySelector('#integrantesTrabajando');
+let cantidadTrabajando = document.querySelector('#integrantesTrabajando');
 
 let $ejecutaCargarTrabajadores = document.querySelector("#cargarIntegrantes");
 
 $ejecutaCargarTrabajadores.onclick = obtieneIntegrantes;
 
 function obtieneIntegrantes() {
-    for(i=0; i<cantidadTrabajando.value; i++){
-      creaFormSalarios(i)
-    }  
-  }
+  for(i=0; i<cantidadTrabajando.value; i++){
+    creaInputSalarios(i)
+  } 
+  const createButton= document.createElement('button')
+  createButton.innerText= 'Cargar Salarios';
+  document.body.appendChild(createButton);
+  
+}
 
-  function crearFormSalarios(i) {
-    let cajaForm = document.querySelector("#integrantesQueTrabajan");
-    let $inputSalario = document.createElement("form");
-    let $numeroDeIntegrante = document.createElement("p");
-    let $saltoDeLinea = document.createElement("br");
-  
-    cajaForm.appendChild($numeroDeIntegrante);
-    $numeroDeIntegrante.textContent = `Ingresar Salario Integrante Trabajador ${i}`;
-    cajaForm.appendChild($inputSalario);
-    cajaForm.appendChild($saltoDeLinea);
-    
-  
-  }
+function creaInputSalarios(i) {
+  let cajaDiv = document.querySelector("#integrantesQueTrabajan");
+  let $inputSalario = document.createElement("input");
+  let $numeroDeIntegrante = document.createElement("p");
+  let $saltoDeLinea = document.createElement("br");
+
+  cajaDiv.appendChild($numeroDeIntegrante);
+  $numeroDeIntegrante.textContent = `Ingresar Salario Integrante Trabajador ${i+1}`;
+  cajaDiv.appendChild($inputSalario);
+  cajaDiv.appendChild($saltoDeLinea);
+
+}
