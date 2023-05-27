56c258341d9ca1afb6001fb0123be950a68c0e6a
Alberto <albert-@live.com.ar>
2023-05-27 12:32:49 -0300

    Se creo index2 para armar la creacion de inputs en index 1 se armo toda la bse ver por qeu no corre la carga del array

--- main.js
+++ main.js
@@ -1,35 +1,76 @@
-//let Integrantes = prompt("Nuemro de Integrantes de la Familia")
+//let integrantes = prompt("Numero de Integrantes de la Familia")
+let listaDeSalarios = [];
 
 function pedirSalarios() {
     const numeroDeTrabajadores = document.getElementById("numeroTrabajadores").value;
-    let salarios = [];
-    let salarioMaximo = 0;
-    let salarioMinimo = 0;
-    let salariototal = 0;
-  
+
     for (let i = 1; +i <= numeroDeTrabajadores; i++) {
       const salario = prompt(`Ingrese el salario del trabajador ${i}:`);
-      salarios.push(parseFloat(salario));
+      listaDeSalarios.push(salario);
 
-      if (salarioMaximo < salarios.salario){
-        salarioMaximo = salarios.salario;
-      }
-
-      if (salarioMinimo > salarios.salario){
-      salarioMinimo = salarios.salario;
-      }
+      console.log(listaDeSalarios);
 
     }
 
-    let salariopromedio = salariototal / numeroDeTrabajadores;
+  }
 
-    const resultado = document.getElementById("resultado");
-    resultado.innerHTML = `
-      Maximo salario: ${salarioMaximo}<br>
-      Minimo salario: ${salarioMinimo}<br>
-      Salario promedio: ${salariopromedio}<br>
-      Total de salarios: ${salariototal}
-
-    `;
+  function ordenarSalarioMaximo(array) {
+    const longitudDelArray = array.length;
     
-}
+    for (let i = 0; i < longitudDelArray; i++) {
+      for (let j = 0; j < longitudDelArray; j++) {
+        if(array[j] > array[j + 1]) {
+          let contenedorTemporal = array[j];
+          array[j] = array[j + 1];
+          array[j + 1] = contenedorTemporal;
+      }
+        
+    }
+   
+    }
+
+  }
+
+  function ordenarSalarioMinimo(array) {
+    const longitudDelArray = array.length;
+    
+    for (let i = 0; i < longitudDelArray; i++) {
+      for (let j = 0; j < longitudDelArray; j++) {
+        if(array[j] < array[j + 1]) {
+          let contenedorTemporal = array[j];
+          array[j] = array[j + 1];
+          array[j + 1] = contenedorTemporal;
+      }
+        
+    }
+   
+    }
+
+  }
+
+  function sumaTotal(array) {
+
+    let sumaSalariosTotal = 0;
+
+    for (let i = 0; i < array.length; i++) {
+
+      sumaSalariosTotal += array[i];
+   
+    }
+
+  }
+
+  function promedioTrabajadores (sumaSalariosTotal, numeroDeTrabajadores) {
+    let promedioSalarioTrabajadores = sumaSalariosTotal / numeroDeTrabajadores;
+  }
+
+  function promedioIntegrantes (sumaSalariosTotal, integrantes)
+  {
+
+    let promedioSalarioIntegrantes = sumaSalariosTotal / integrantes;
+
+
+  }
+
+    
+  
