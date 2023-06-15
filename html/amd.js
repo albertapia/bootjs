a5ede893f75602f39a468549e6edeb6aef33ed44
Alberto <albert-@live.com.ar>
2023-06-15 20:37:01 -0300

    Se organizo todo en 2 carpetas, se elimino archivos de pruebas, Estamos en el tramo final del primer Ejercicio

--- html/amd.js
+++
@@ -1,9 +0,0 @@
-let anio = prompt ("Ingrese el año (formato AAAA): ");
-let mes = prompt("Ingrese el mes (formato MM): ");
-let dia = prompt("Ingrese el dia (formato DD): ");
-
-let AMD = anio + mes + dia; //AMD acronimo Año Mes Dia
-
-document.write(AMD);
-console.log(AMD);
-
--- main.js
+++ main.js
@@ -1,48 +1,122 @@
-//let integrantes = prompt("Numero de Integrantes de la Familia")
+const botonCargarTrabajadores = document.querySelector('#botonCargarTrabajadores');
+const divResultado = document.querySelector("#divResultado");
 
-const botonCargarTrabajadores = document.getElementById("botonCargarTrabajadores");
-const resultado = document.getElementById("resultado");
+const divInputs = document.querySelector('#divInputs')
+const divBotonEscondido = document.querySelector('#divBotonEscondido')
+const botonCargarSalarios = document.querySelector('#botonCargarSalarios')
 
-botonCargarTrabajadores.addEventListener("click", crearInputs);
+let salariosCargados = [];
+
+botonCargarTrabajadores.onclick = crearInputs;
+botonCargarSalarios.onclick = cargarSalarios;
 
 function crearInputs() {
+  const inputs = document.querySelectorAll("#divInputs input[type='number']");
+  for (let i = 0; i < inputs.length; i++) {
+    inputs[i].parentNode.removeChild(inputs[i]);
+  }
 
-  const numeroDeTrabajadores = document.getElementById("numeroDeTrabajadores").value;
+  const numeroDeTrabajadores = document.querySelector("#numeroDeTrabajadores").value;
 
-	for (let i = 0; i < numeroDeTrabajadores; i++) {
-		const input = document.createElement("input");
-		input.type = "number";
-		input.placeholder = "Salario del trabajador #" + (i+1);
-		input.id = "input-" + i;
-		document.body.appendChild(input);
-	}
-
-	const botonCargar = document.createElement("button");
-	botonCargar.innerText = "Cargar salarios";
-	botonCargar.id = "botonCargarSalarios";
-	botonCargar.onclick = botonCargarTrabajadores;
-	document.body.appendChild(botonCargar);
+  for (let i = 0; i < numeroDeTrabajadores; i++) {
+    const input = document.createElement("input");
+    input.type = "number";
+    input.placeholder = "Salario del trabajador #" + (i + 1);
+    input.className = "salario-integrante";
+    divInputs.appendChild(input);
+  }
+  divInputs.classList.remove('hide');
+  divBotonEscondido.classList.remove('hide');
+  divResultado.classList.add('hide');
+  
 }
 
-const botonCargarSalarios = document.getElementById("botonCargarSalarios");
-botonCargarSalarios.addEventListener("click", cargarSalarios);
+function cargarSalarios() {
+  const salarios = document.querySelectorAll('.salario-integrante');
+  salariosCargados = [];
+
+  for (let i = 0; i < salarios.length; i++) {
+    salariosCargados.push(Number(salarios[i].value));
+  }
+  console.log(salariosCargados);
+
+  divBotonEscondidoResultado.classList.remove('hide');
+  divResultado.classList.remove('hide');
+}
+
+const botonResultados = document.querySelector('#botonResultados');
+
+botonResultados.onclick = resultados;
+
+function resultados() {
+  const longitudDelArray = salariosCargados.length;
+
+  ordenadoMayorMenor();
+  sumaTotal();
+  calcularPromedio();
+
+  /*
+  divResultado.innerHTML = `
+    Maximo salario: ${salariosCargados[0]}<br>
+    Minimo salario: ${salariosCargados[longitudDelArray - 1]}<br>
+    Suma de salarios ${suma}<br>
+    Salario promedio: ${promedio}<br>
+    
+  `;
+
+  */
+  botonResultados.classList.add('hide');
 
 
-function cargarSalarios () { 
-	const numeroDeTrabajadores = document.getElementById("numeroDeTrabajadores").value;
-	let salarios = []
+}
 
-	for (let i = 0; i < numeroDeTrabajadores; i++) {
-		const input = document.getElementById("input-" + i);
-		salarios.push(parseFloat(input.value));
-	}
+function ordenadoMayorMenor() {
+  const longitudDelArray = salariosCargados.length;
 
-	// Supuestamente esto elimina los inputs creados con la funcion crearimputs
-	const inputs = document.querySelectorAll("input[type='number']");
-	for (let i = 0; i < inputs.length; i++) {
-		inputs[i].parentNode.removeChild(inputs[i]);
-	}
-	
-	consola.log(salarios);
+  for (let i = 0; i < longitudDelArray; i++) {
+    for (let j = 0; j < longitudDelArray - i - 1; j++) {
+      if (salariosCargados[j] < salariosCargados[j + 1]) {
+        let contenedorTemporal = salariosCargados[j];
+        salariosCargados[j] = salariosCargados[j + 1];
+        salariosCargados[j + 1] = contenedorTemporal;
+      }
+    }
+  }
+  console.log(salariosCargados);
+  console.log("El mayor salario es: " + salariosCargados[0]);
+  console.log("El menor salario es: " + salariosCargados[longitudDelArray - 1]);
+
+  maximoSalario.textContent = `El mayor salario es: ${salariosCargados[0]}`;
+  minimoSalario.textContent = `El menor salario es: ${salariosCargados[longitudDelArray - 1]}`;
+
+  //divResultado.textContent = (`El menor numero es: ${salariosCargados[longitudDelArray - 1]}`);
+  //divResultado.textContent = `El mayor número es: ${salariosCargados[0]}`;
+
+
+}
+
+function sumaTotal() {
+  let suma = 0;
+
+  for (let i = 0; i < salariosCargados.length; i++) {
+    suma += salariosCargados[i];
+  }
+
+  console.log("Suma total de salarios:", suma);
+  sumaSalario.innerHTML = "Suma total de salarios: " + suma;
+
+}
+
+function calcularPromedio() {
+  let suma = 0;
+
+  for (let i = 0; i < salariosCargados.length; i++) {
+    suma += salariosCargados[i];
+  }
+
+  let promedio = suma / salariosCargados.length;
+
+  console.log("Promedio de salarios:", promedio);
+  promedioSalario.innerHTML = "Promedio de salarios: " + promedio;
 
 }
