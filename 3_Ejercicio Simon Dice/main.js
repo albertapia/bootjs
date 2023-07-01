ab2a345f93a70deec2dae776c669384ba3b65c21
Alberto <albert-@live.com.ar>
2023-07-01 19:50:55 -0300

    [CREATE & FIX] Se arreglo el inicio del juego se crearon las funciones necesarias desde le punto de vista actual comentando tareas especificas a averiguar para implementar

--- 3_Ejercicio Simon Dice/main.js
+++ 3_Ejercicio Simon Dice/main.js
@@ -1,4 +1,3 @@
-let $titulo = document.querySelector('#titulo');
 const $botonStar = document.querySelector('#botonStart');
 const $botonRojo = document.querySelector('#rojo');
 const $botonAmarillo = document.querySelector('#amarillo');
@@ -6,22 +5,68 @@ const $botonVerde = document.querySelector('#verde');
 const $botonAzul = document.querySelector('#azul');
 const cantidadDeBotonesDeColor = document.querySelectorAll('botonCaja');
 
-let patron = []; /*  En este array tengo que pushear el patron cuando se vaya avanzando*/
+let secuenciaJuego = []; /*  En este array tengo que pushear el patron cuando se vaya avanzando en el juego*/
+let secuenciaJugador = []; /*  En este array tengo que pushear el patron cuando el jugador vaya avanzando en el juego para comparar con el otro array*/
+//let puntajeTotal = 0;
+//let puntajeMaximo = 0; probando donde iniciar estas dos variables en 0
+let juegoIniciado = false;
 
 $botonStar.onclick = start;
 $botonRojo.onclick = botonRojo;
-/* $botonAmarillo.onclick = botonAmarillo;
+$botonAmarillo.onclick = botonAmarillo;
 $botonVerde.onclick = botonVerde;
-$botonAzul.onclick = botonAzul; */ //Estoy en fase de prueba del boton rojo y random de momento
+$botonAzul.onclick = botonAzul;
 
 function start() {
-    botonRandom ();
+    juegoIniciado = true;
+    if (juegoIniciado) {
+        juegoIniciado = true;
+        iniciarJuego();
+      }
+    }
 
-}
-
-function botonRandom (){
+/*function botonRandom (){
 
     let randomBoton = Math.floor(Math.random() * cantidadDeBotonesDeColor.length);
     return  [randomBoton];
+}*/ // Prueba Fallo dejo el codigo comentado como nota de momento.
+
+function botonRojo() {}
+function botonAmarillo() {}
+function botonVerde() {}
+function botonAzul() {}
+
+function iniciarJuego () {
+    let puntajeTotal = 0;
+    // let puntajeMaximo = 0; /* Se comento por que el puntaje maximo no se tiene que inicializar en 0 nuevamente cada vez que se inicia el juego creo*/
+    let secuenciaJuego = []; 
+    let secuenciaJugador = [];
+
+    secuenciaDelJuego();
 }
 
+function secuenciaDelJuego () {
+    let botonRandom = Math.floor(Math.random()*4);
+    secuenciaJuego.push(botonRandom);
+
+    mostrarSecuencia();
+
+    puntajeTotal++;
+    puntajeMaximo++;
+}
+
+function mostrarSecuencia(){}
+
+function iluminarBoton(){
+    //Aca se usan muchjos if ver si se pueden usar case pero seguramente if mas ifelse revisar eso
+}
+
+function validarSecuencia (){
+    //aca se compara las dos secuencias bah los dos array si son iguales se continua llamando a siguienteSecuencia o al nombre que le haya puesto
+}
+
+function gameOver() {
+    //aca se pone el tru del juego inicoado a false para que termine se muestra en el documento algo que diga se perdio
+}
+
+/* Ver donde poner el classlist.add y remove hide para iluminar el boton e investigar si se puede hacer con un relee*/
