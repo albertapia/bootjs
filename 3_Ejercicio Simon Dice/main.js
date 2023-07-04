cd38a6e13909c9d2dbcce4a22cdfe77bc5f7217c
Alberto <albert-@live.com.ar>
2023-07-04 02:21:40 -0300

    [modify] se modifico el rumbo del js a prueba y error

--- 3_Ejercicio Simon Dice/main.js
+++ 3_Ejercicio Simon Dice/main.js
@@ -3,51 +3,72 @@ const $botonRojo = document.querySelector('#rojo');
 const $botonAmarillo = document.querySelector('#amarillo');
 const $botonVerde = document.querySelector('#verde');
 const $botonAzul = document.querySelector('#azul');
-const cantidadDeBotonesDeColor = document.querySelectorAll('botonCaja');
+//const cantidadDeBotonesDeColor = document.querySelectorAll('botonCaja');
 
-let secuenciaJuego = []; /*  En este array tengo que pushear el patron cuando se vaya avanzando en el juego*/
-let secuenciaJugador = []; /*  En este array tengo que pushear el patron cuando el jugador vaya avanzando en el juego para comparar con el otro array*/
-//let puntajeTotal = 0;
-//let puntajeMaximo = 0; probando donde iniciar estas dos variables en 0
+let secuenciaDelJuego = []; /*  En este array tengo que pushear el patron cuando se vaya avanzando en el juego*/
+let secuenciaDelJugador = []; /*  En este array tengo que pushear el patron cuando el jugador vaya avanzando en el juego para comparar con el otro array*/
+//let puntajeTotal = 0; probando donde iniciar estas dos variables en 0
+let puntajeMaximo = 0; 
 let juegoIniciado = false;
 
-$botonStar.onclick = start;
+const colores = ["rojo", "amarillo", "verde", "azul"];
+
+$botonStar.onclick = iniciandoElJuego;
 $botonRojo.onclick = botonRojo;
 $botonAmarillo.onclick = botonAmarillo;
 $botonVerde.onclick = botonVerde;
 $botonAzul.onclick = botonAzul;
 
-function start() {
-    juegoIniciado = true;
-    if (juegoIniciado) {
+function iniciandoElJuego() {
+    if (juegoIniciado = false) {
         juegoIniciado = true;
         iniciarJuego();
       }
     }
-
 /*function botonRandom (){
-
-    let randomBoton = Math.floor(Math.random() * cantidadDeBotonesDeColor.length);
-    return  [randomBoton];
+ let randomBoton = Math.floor(Math.random() * cantidadDeBotonesDeColor.length);
+ return  [randomBoton];
 }*/ // Prueba Fallo dejo el codigo comentado como nota de momento.
 
-function botonRojo() {}
-function botonAmarillo() {}
-function botonVerde() {}
-function botonAzul() {}
+function botonRojo() {
+    if (juegoIniciado=true) {
+        secuenciaJugador.push(0);
+        verificarSecuencia();
+      }
+}
+
+function botonAmarillo() {
+    if (juegoIniciado=true) {
+        secuenciaJugador.push(1);
+        verificarSecuencia();
+      }
+}
+
+function botonVerde() {
+    if (juegoIniciado=true) {
+        secuenciaJugador.push(2);
+        verificarSecuencia();
+      }
+}
+
+function botonAzul() {
+    if (juegoIniciado=true) {
+        secuenciaJugador.push(3);
+        verificarSecuencia();
+      }
+}
 
 function iniciarJuego () {
     let puntajeTotal = 0;
-    // let puntajeMaximo = 0; /* Se comento por que el puntaje maximo no se tiene que inicializar en 0 nuevamente cada vez que se inicia el juego creo*/
-    let secuenciaJuego = []; 
-    let secuenciaJugador = [];
-
-    secuenciaDelJuego();
+    // let puntajeMaximo = 0; /* Se comento por que el puntaje maximo no se tiene que inicializar en 0 nuevamente cada vez que se inicia el juego creo
+    secuenciaDelJuego = []; 
+    secuenciaDelJugador = [];
+    siguienteSecuencia();
 }
 
-function secuenciaDelJuego () {
+function siguienteSecuencia() {
     let botonRandom = Math.floor(Math.random()*4);
-    secuenciaJuego.push(botonRandom);
+    secuenciaDelJuego.push(botonRandom);
 
     mostrarSecuencia();
 
@@ -55,14 +76,29 @@ function secuenciaDelJuego () {
     puntajeMaximo++;
 }
 
+
 function mostrarSecuencia(){}
 
+/*
 function iluminarBoton(){
     //Aca se usan muchjos if ver si se pueden usar case pero seguramente if mas ifelse revisar eso
 }
 
 function validarSecuencia (){
     //aca se compara las dos secuencias bah los dos array si son iguales se continua llamando a siguienteSecuencia o al nombre que le haya puesto
+    if (secuenciaJugador)
+
+
+    if (secuenciaJugador[secuenciaJugador.length - 1] !== sequence[playerSequence.length - 1]) {
+        gameOver();
+        return;
+      }
+    
+      if (secuenciaJugador.length === secuenciaJuego.length) {
+        playerSequence = [];
+        setTimeout(function() {
+          nextSequence();
+        }, 1000);
 }
 
 function gameOver() {
