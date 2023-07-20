edd8ccbc1416ed297f60fbee904f1ef14a59fd2e
Alberto <albert-@live.com.ar>
2023-07-20 19:59:01 -0300

    Modify html se agraron divs para los turnos, css, se agrego estilos para los nuevos divs, js se separo mas las funciones para hacerlas mas especificas y simples y se crearon un par de funciones para unir todo y ejecutar las mismas

--- 3_Ejercicio Simon Dice/main.js
+++ 3_Ejercicio Simon Dice/main.js
@@ -2,6 +2,9 @@ const $botones = document.querySelectorAll('.botonCaja');
 const $botonEmpezar = document.querySelector('.botonStart');
 const $cajaScore = document.getElementById('cajaScore');
 const $cajaScoreMaximo = document.getElementById('cajaScoreMaximo');
+const $cajaCpu = document.getElementById('cpu');
+const $cajaHuman = document.getElementById('human');
+
 let secuenciaDelJuego = []; // secuencia del juego ...MAKINA
 let secuenciaDelJugador = []; // secuencia del HUMANO
 let puntajeJuegoActual = 0; // Puntaje del juego actual
@@ -16,40 +19,69 @@ $cuadradoBotones.addEventListener('click', ()=> {
     console.log("Hago click")
 })*/
 
-function empezarJuego() {
-  $botonEmpezar.disabled = true; // para desabilitar el boton una vez iniciado
-  secuenciaDelJuego = []; 
-  puntajeJuegoActual = 0; 
-  secuenciaDelJugador = [];
-  agregarNuevoColor();
+function juego () {
+  empezarJuego();
+  turnoCpu();
+  turnoHumano();
+  turnoLogica();
+
+  iluminarColor(); //ver donde meterlo
+
 }
 
-function agregarNuevoColor() {
-  const nuevoColor = obtenerColorAleatorio();
+function empezarJuego() {
+  $botonEmpezar.disabled = true;
+  secuenciaDelJuego = []; 
+  puntajeJuegoActual = 0; 
+  secuenciaDelJugador = []; 
+}
+
+function turnoCpu(){
+  $botones.disabled = true;
+  $cajaHuman.classList.remove('iluminado');
+  $cajaIa.classList.add('iluminado');
+  obtenerColorAleatorio();
+  agregarNuevoColor();
+  mostrarSecuencia();
+}
+
+function turnoHumano(){
+  $botones.disabled = false;
+  $cajaIa.classList.remove('iluminado');
+  $cajaHuman.classList.add('iluminado');
+  let secuenciaDelJugador = [];
+  secuenciaDelJugador = $botones
+
+  //averiguar como poner cierta cantidad de segfundos o milisegundos para que aprete las cosas
+
+}
+
+function turnoLogica(){
+  verificarSecuencia ()
+}
+
+function agregarNuevoColor(nuevoColor) {
   secuenciaDelJuego.push(nuevoColor);
-  mostrarSecuencia(secuenciaDelJuego);
 }
 
 function obtenerColorAleatorio() {
   const coloresDisponibles = ['rojo', 'verde', 'azul', 'amarillo'];
-  const color = Math.floor(Math.random() * coloresDisponibles.length);
-  return coloresDisponibles[color];
+  const nuevoColor = Math.floor(Math.random() * coloresDisponibles.length);
+  return coloresDisponibles[nuevoColor];
 }
 
 function mostrarSecuencia() {
-  let i = 0;
-  const intervalo = setInterval(() => {
-    const color = secuencia[i];
-    iluminarColor(color);
-    i++;
-    if (i >= secuencia.length) {
-      clearInterval(intervalo);
+  for (let i = 0; i < secuenciaDelJuego.length; i++) {
+      const color = secuenciaDelJuego[i];
+      setTimeout(() => {
+          iluminarColor(color);
+      }, i * 1000);
+  }
+
+  setTimeout(() => {
       permitirJugada();
-    }
-  }, 1000);
+  }, secuenciaDelJuego.length * 1000);
 }
-
-
 
 function iluminarColor(color) {
   const $botonIluminado = document.getElementById(color); 
@@ -60,43 +92,44 @@ function iluminarColor(color) {
   }, 500); 
 }
 
-  function verificarSecuencia () {
-    if (secuenciaDelJugador[secuenciaDelJugador.length - 1] != secuenciaDelJuego[secuenciaJugador.length - 1]) {
-      finDelJuego();
-      return;
-    }
-  
-    if (secuenciaDelJugador.length === secuenciaDelJuego.length) {
-      secuenciaDelJugador = [];
-      setTimeout(siguienteSecuencia, 1000);
-    }
+function permitirJugada() {
+  // Aca tendria qeu poner algo que permita que el jugador ingrese su secuencia creo
+}
 
+function verificarSecuencia () {
+  if (secuenciaDelJugador[secuenciaDelJugador.length - 1] != secuenciaDelJuego[secuenciaJugador.length - 1]) {
+    finDelJuego();
+    return;
   }
+  
+  if (secuenciaDelJugador.length === secuenciaDelJuego.length) {
+    secuenciaDelJugador = [];
+    setTimeout( turnoCpu, 1000); // aca va otro mas creo parametro definir bien las cosas
+  }
+
+}
 
   function finDelJuego () {
     // agergar los respectivos inerhtml o contenttex para mostar el puntaje maximo y que este quede si continua jugando y ver si el reinicio que el puntaje total se reinicie en 0 este donde corresponda
 
-  cajaScore.classList.remove('hide');
-  cajaScoreMaximo.classList.remove('hide');
-  
-
+    cajaScore.classList.remove('hide');
+    cajaScoreMaximo.classList.remove('hide');
     puntajeFinDelJuego();
     maximoPuntaje();
 
-    juegoIniciado = false;
   }
+
+  function puntajeFinDelJuego() {
+    
+    $cajaScore.textContent = `Puntaje: ${puntajeJuegoActual}`;
+}
   
   function maximoPuntaje (){
     if (puntajeTotal > puntajeMaximo) {
       puntajeMaximo = puntajeTotal;
     }
-    puntajeMaximo.textContent = `Puntaje Maximo: ${puntajeTotal}`;
+    $cajaScoreMaximo.textContent = `Puntaje Maximo: ${puntajeTotal}`;
 
-
-  }
-
-  function puntajeFinDelJuego () {
-    puntajeJuegoActual.textContent = `Puntaje: ${puntajeTotal}`;
 
   }
 
