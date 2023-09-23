409970fd3d1db5ec477a2f4fd7f92a65793507be
Alberto <albert-@live.com.ar>
2023-09-23 20:39:20 -0300

    Se modifico muchas cosas se agrego resaltar colores con ayuda de youtube ... estoy buscando el error o que mas modificar

--- 3_Ejercicio Simon Dice/main.js
+++ 3_Ejercicio Simon Dice/main.js
@@ -1,27 +1,119 @@
 const colores = ["rojo", "amarillo", "verde", "azul"];
 const cajaBotones = document.querySelector("#cajaBotones");
-const botonStart = document.querySelector(".botonStart");
+const botonStart = document.querySelector("#botonStart");
 const puntajeJuegoActual = document.querySelector("#puntajeJuegoActual");
 const puntajeMaximo = document.querySelector("#puntajeMaximo");
-const cajaScore = document.querySelector("#cajaScore");
-const cajaScoreMaximo = document.querySelector("#cajaScoreMaximo");
-const cajaUserCPU = document.querySelector("#cpu");
-const cajaUserHumano = document.querySelector("#human");
+const botones = document.querySelectorAll(".botonCaja");
+
+botonStart.onclick = empezarJuego;
+//botonStart.addEventListener("click", empezarJuego);
+//cajaBotones.onclick = manejarClick;
+cajaBotones.addEventListener("click", manejarClick);
 
 let secuenciaMaquina = [];
 let secuenciaJugador = [];
 let puntaje = 0;
 let jugadaUsuarioHabilitada = false;
 
-botonStart.addEventListener("click", empezarJuego);
-cajaBotones.addEventListener("click", manejarClick);
-
 function empezarJuego() {
-    secuenciaMaquina = [];
-    secuenciaJugador = [];
-    nivel = 1;
-    puntaje = 0;
-    puntajeJuegoActual.textContent = "Puntaje: " + puntaje;
-    cajaScore.classList.add("hide");
+    reiniciarJuego();
     generarSecuencia();
 }
+
+function reiniciarJuego() {
+    secuenciaMaquina = [];
+    secuenciaJugador = [];
+    puntaje = 0;
+    puntajeJuegoActual.textContent = "Puntaje: " + puntaje;
+}
+
+function generarSecuencia() {
+    reiniciarInputUsuario();
+    const nuevoColor = colores[Math.floor(Math.random() * 4)];
+    secuenciaMaquina.push(nuevoColor);
+    mostrarSecuencia();
+}
+
+function mostrarSecuencia() {
+    let i = 0;
+    const intervalo = setInterval(() => {
+        resaltarColor(secuenciaMaquina[i]);
+        i++;
+
+        if (i >= secuenciaMaquina.length) {
+            habilitarInputUsuario();
+            clearInterval(intervalo);
+        }
+    }, 1000);
+}
+
+
+//la funcion resaltar color no pude hacerla la saque de un video de youtube
+function resaltarColor(color) {
+    const boton = document.querySelector(`#${color}`);
+    boton.style.boxShadow = "0px 0px 30px white";
+
+    setTimeout(() => {
+        boton.style.boxShadow = "";
+    }, 500);
+}
+
+function reiniciarInputUsuario() {
+    jugadaUsuarioHabilitada = false;
+}
+
+function habilitarInputUsuario() {
+    jugadaUsuarioHabilitada = true;
+}
+
+function manejarClick(event) {
+    if (!jugadaUsuarioHabilitada) {
+        return;
+    }
+
+    const botonClickeado = event.target;
+
+    if (botonClickeado.classList.contains("botonCaja")) {
+        const colorClickeado = botonClickeado.id;
+        resaltarColor(colorClickeado);
+        secuenciaJugador.push(colorClickeado);
+
+        const jugadaCorrecta = verificarJugada();
+
+        if (jugadaCorrecta) {
+            if (secuenciaJugador.length === secuenciaMaquina.length) {
+                puntaje++;
+                puntajeJuegoActual.textContent = "Puntaje: " + puntaje;
+                nivel++;
+                setTimeout(generarSecuencia, 1000);
+            }
+        } else {
+            finDelJuego();
+        }
+    }
+}
+
+function verificarJugada() {
+    if (secuenciaJugador.length !== secuenciaMaquina.length) {
+        return false;
+    }
+
+    for (let i = 0; i < secuenciaJugador.length; i++) {
+        if (secuenciaJugador[i] !== secuenciaMaquina[i]) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+
+function finDelJuego() {
+    if (puntaje > parseInt(puntajeMaximo.textContent)) {
+        puntajeMaximo.textContent = puntaje;
+    }
+
+    reiniciarJuego();
+    reiniciarInputUsuario();
+}
+
